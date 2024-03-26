2fbd74f31628e99f59b5b8d47c97e3b26772aeb0
Mhijazi16 <gultimate0@gmail.com>
2024-03-26 21:29:33 +0200

    create nicknames list nad clients list

--- server.py
+++ server.py
@@ -5,3 +5,9 @@ import threading
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(('127.0.0.1',6969))
 server.listen()
+
+print("SERVER IS LISTENING !!!")
+
+nicknames: list[str] = []
+clients: list[socket.socket] = []
+
